91f9ef0eb1a1c204211876ae76a594913e7c867c
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2024-03-31 08:41:02 -0600

    Moved the gallery related views into their own group

--- LetUsCook/View/RecipeGallery/RecipeGalleryView.swift
+++ LetUsCook/View/RecipeGallery/RecipeGalleryView.swift
@@ -26,33 +26,6 @@ struct RecipeGalleryView: View {
     }
 }
 
-struct RecipeGallery: View {
-    var recipes: [Recipe]
-//    @Binding var editing: Bool
-//    let selectRecipe: (Recipe) -> Void
-//    let addRecipe: () -> Void
-
-    var body: some View {
-        Text("Recipe Gallery")
-        
-        Table(recipes) {
-            TableColumn("Name", value: \.name)
-            TableColumn("Prep Time", value: \.prepTime)
-            TableColumn("Cook Time", value: \.cookTime)
-            TableColumn("Comments", value: \.comments)
-        }
-        
-        
-//        ScrollView {
-//            LazyVGrid(
-//                columns: RecipeGallery([GridItem(200)]),
-//                spacing: 20
-//            ) {
-//                }
-//            }
-    }
-}
-
 #Preview {
     RecipeGalleryView()
 }
